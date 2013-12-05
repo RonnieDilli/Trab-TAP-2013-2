2d8443eaf7e27a9eac0543635dd098f19947e63e
Ronnie Dilli <guribao@gmail.com>
2013-12-04 23:01:18 -0200

    Adicionados comentários no Classe 'Programa' (main).

--- src/Programa.java
+++ src/Programa.java
@@ -1,18 +1,23 @@
 public class Programa {
 	public static void main(String[] args) {
+    // Instancia o Servidor a ser decorado.
 		Servidor sv = new Servidor();
 		
-		CompiladorFactory cf = new CompiladorFactory();
-		EmpacotadorFactory ef = new EmpacotadorFactory();
-		PublicadorFactory pf = new PublicadorFactory();
+    // Instancia as Fabricas.
+		CompiladorFactory cfactory = new CompiladorFactory();
+		EmpacotadorFactory efactory = new EmpacotadorFactory();
+		PublicadorFactory pfactory = new PublicadorFactory();
 		
-		CompiladorDecorator compilador = (CompiladorDecorator) cf.criaServidor(sv);
-		EmpacotadorDecorator empacotador = (EmpacotadorDecorator) ef.criaServidor(sv);
-		PublicadorDecorator publicador = (PublicadorDecorator) pf.criaServidor(sv);
+    // Gera as instancias dos Decoradores e Decora o Servidor 'sv'.
+		CompiladorDecorator compilador = (CompiladorDecorator) cfactory.criaServidor(sv);
+		EmpacotadorDecorator empacotador = (EmpacotadorDecorator) efactory.criaServidor(sv);
+		PublicadorDecorator publicador = (PublicadorDecorator) pfactory.criaServidor(sv);
 		
+    // Atribui a Cadeia de Responsabilidade.
 		compilador.setNext(empacotador);
 		empacotador.setNext(publicador);
 		
+    // Inicia a execucao.
 		compilador.executa();
 	}
 }
